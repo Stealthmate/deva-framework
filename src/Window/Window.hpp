5647eb790ba1808df02aa1584860978dd452714b
Stealthmate <Stealthmate@users.noreply.github.com>
2015-09-03 14:28:30 +0300

    test1

--- src/Window/Window.hpp
+++ src/Window/Window.hpp
@@ -24,7 +24,7 @@ namespace DevaFramework
 
 	public:
 
-		///Set the context, routing all draw calls to the specified window
+		///Set the context, routing all draw calls to the specified windowasdasd
 		static DEVA_WINDOW_API void setCurrentWindow(const Window &wnd);
 		///Create a Window with the given parameters
 		static DEVA_WINDOW_API Window& createWindow(
