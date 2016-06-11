eabfebb5e2cf84795e4caf7e687b9161493c6ae4
Stealthmate <stealthmate1233@gmail.com>
2016-06-12 02:54:55 +0300

    Added TWOPI as constant

--- src/Math/Config.hpp
+++ src/Math/Config.hpp
@@ -21,6 +21,7 @@ namespace DevaFramework
 {
 	const float EPS = 1.0E-10f;
 	const double M_PI = 3.1415;
+	const double TWOPI = 2 * M_PI;
 
 	///Converts degrees to radians
 	inline float rad(float deg)
