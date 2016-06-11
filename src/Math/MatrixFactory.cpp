5c69101ebc0405abd1b283900a6547f1d768bc2a
Stealthmate <stealthmate1233@gmail.com>
2016-06-11 23:56:43 +0300

    Fixed computation error in rotate()
     - For some reason it performed a Z axis rotation, ignoring the axis
       parameter

--- src/Math/MatrixFactory.cpp
+++ src/Math/MatrixFactory.cpp
@@ -6,11 +6,17 @@ using namespace DevaFramework;
 mat4 DevaFramework::rotate(float angle, vec3 axis)
 {
 	vec3 axis_normal = axis.normal();
+
+	float c = std::cos((double)angle);
+	float s = std::sin((double)angle);
+	float x = axis_normal[0];
+	float y = axis_normal[1];
+	float z = axis_normal[2];
 	return mat4
 	{ 
-		vec4{cos(angle), 0, sin(angle), 0}, 
-		vec4{0, 1, 0, 0}, 
-		vec4{-sin(angle), 0, cos(angle), 0}, 
+		vec4{ c + ((x*x)*(1-c)),     (x*y*(1 - c)) - (z*s), (x*z*(1 - c)) + (y*s), 0}, 
+		vec4{ (x*y*(1 - c)) + (z*s), c + ((y*y)*(1 - c)),     (y*z*(1 - c)) - (x*s), 0},
+		vec4{ (x*z*(1 - c)) - (y*s), (z*y*(1 - c)) + (x*s), c + ((z*z)*(1 - c)), 0},
 		vec4{0, 0, 0, 1}
 	};
 }
