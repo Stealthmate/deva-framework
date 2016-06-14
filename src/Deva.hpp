069db47a1180f5daa2f9751578de77062537e670
Stealthmate <stealthmate1233@gmail.com>
2016-06-14 23:27:17 +0300

    For some reason this was outdated

--- src/Deva.hpp
+++ src/Deva.hpp
@@ -9,49 +9,53 @@
 #include "Window/Master.hpp"
 #include "Graphics/Master.hpp"
 
-int __deva_init_util() 
-{
-	DevaFramework::Logger::println("Initializing Util...");
-	return DevaFramework::DEVA_INIT_UTIL();
-}
-
-int __deva_init_math() 
-{
-	DevaFramework::Logger::println("Initializing Math...");
-	return DevaFramework::DEVA_INIT_MATH();
-}
-
-int __deva_init_graphics() 
-{
-	DevaFramework::Logger::println("Initializing Graphics...");
-	return DevaFramework::DEVA_INIT_GRAPHICS();
-}
-int __deva_init_window()
-{
-	DevaFramework::Logger::println("Initializing Window...");
-
-	int success = DevaFramework::DEVA_INIT_WINDOW();
-
-	switch (success) {
-		case 1:
-		{
-			DevaFramework::Logger::err.println("Could not load GLFW.");
-		} break;
-	}
-	return DevaFramework::DEVA_INIT_WINDOW();
-}
 
 namespace DevaFramework
 {
-	int DEVA_INIT() 
-	{
+	namespace {
 
-		DevaFramework::Logger::println("Initializing Deva...");
-		if (__deva_init_util() != 0) return 1;
-		if (__deva_init_math() != 0) return 2;
-		if (__deva_init_graphics() != 0) return 3;
-		if (__deva_init_window() != 0) return 4;
-		return 0;
+		int __deva_init_util()
+		{
+			DevaFramework::Logger::println("Initializing Util...");
+			return DevaFramework::DEVA_INIT_UTIL();
+		}
+
+		int __deva_init_math()
+		{
+			DevaFramework::Logger::println("Initializing Math...");
+			return DevaFramework::DEVA_INIT_MATH();
+		}
+
+		int __deva_init_graphics()
+		{
+			DevaFramework::Logger::println("Initializing Graphics...");
+			return DevaFramework::DEVA_INIT_GRAPHICS();
+		}
+		int __deva_init_window()
+		{
+			DevaFramework::Logger::println("Initializing Window...");
+
+			int success = DevaFramework::DEVA_INIT_WINDOW();
+
+			switch (success) {
+			case 1:
+			{
+				DevaFramework::Logger::err.println("Could not load GLFW.");
+			} break;
+			}
+			return DevaFramework::DEVA_INIT_WINDOW();
+		}
+
+		int DEVA_INIT()
+		{
+
+			DevaFramework::Logger::println("Initializing Deva...");
+			if (__deva_init_util() != 0) return 1;
+			if (__deva_init_math() != 0) return 2;
+			if (__deva_init_graphics() != 0) return 3;
+			if (__deva_init_window() != 0) return 4;
+			return 0;
+		}
 	}
 }
 
