d1266a8f5b88d89d154131f616bc519d1da7fd4d
Stealthmate <stealthmate1233@gmail.com>
2016-06-17 18:42:06 +0300

    Updated tests with new API

--- tests/Vulkan/test-Vulkan.cpp
+++ tests/Vulkan/test-Vulkan.cpp
@@ -1,9 +1,27 @@
-#define DEVA_FRAMEWORK_MASTER_H
-#include "../src/Window/Window_new.hpp"
+#include "../src/Deva.hpp"
 
 using namespace DevaFramework;
 
+struct MyListener : public WindowEventListener
+{
+	virtual bool onKeyEvent(WindowEventStruct_KeyEvent info)
+	{
+		return true;
+	}
+};
+
 int main()
 {
-	Window &wnd = Window::openWindow(800, 600, "Vulkan!");
+	try
+	{
+		auto lstnr = std::shared_ptr<MyListener>(new MyListener());
+		Window &wnd = Window::openWindow(800, 600, "Deva: test-Window");
+		wnd.getEventObserver().attachListener(
+		{WindowEvent::KEY_DOWN, WindowEvent::KEY_UP}, std::move(lstnr));
+		while (wnd.update());
+	}
+	catch (DevaFailureException &ex)
+	{
+		DevaLogger::err.println(ex.what());
+	}
 }
--- tests/Window/test-Window.cpp
+++ tests/Window/test-Window.cpp
@@ -1,10 +1,49 @@
 #include "../../src/Deva.hpp"
 
-namespace DEVA_TEST_WINDOW {
+#include "../src/Deva.hpp"
 
-	const char* TEST_DESCRIPTION = 
+using namespace DevaFramework;
+namespace
+{
+	const char* TEST_DESCRIPTION =
 		R"description(Test Description:
 	This test should open an empty window. The window should close when Escape is pressed.)description";
+
+	struct MyListener : public WindowEventListener
+	{
+		virtual bool onKeyEvent(Window &wnd, WindowEventStruct_KeyEvent info)
+		{
+			if(info.key == Key::KEY_ESCAPE) wnd.close();
+			return true;
+		}
+	};
+}
+
+
+int main()
+{
+	DEVA_INIT();
+
+	DevaLogger::log.println(TEST_DESCRIPTION);
+
+	Window &wnd = Window::openWindow(800, 600, "Deva: test-Window");
+	DevaLogger::log.println("Opened window.");
+
+	auto lstnr = std::shared_ptr<MyListener>(new MyListener());
+	wnd.getEventObserver().attachListener(WindowEvent::KEY_DOWN, std::move(lstnr));
+	DevaLogger::log.println("Attached listener.");
+
+
+	while (wnd.update());
+
+	DevaLogger::log.println("Test successful.");
+}
+
+
+/*
+namespace DEVA_TEST_WINDOW {
+
+	
 
 	using namespace DevaFramework;
 
@@ -18,9 +57,6 @@ using namespace DEVA_TEST_WINDOW;
 
 int main()
 {
-	DEVA_INIT();
-
-	DevaLogger::log.println(TEST_DESCRIPTION);
 
 	Window &wnd = Window::createWindow(800, 600, "test-Window");
 	Window::setCurrentWindow(wnd);
@@ -33,4 +69,4 @@ int main()
 
 	DevaLogger::log.println("Test successful.");
 	DevaLogger::log.println("Terminating");
-}
+}*/
