a5b7ca2ad329255b26c7024fb237fea7540cda53
Stealthmate <stealthmate1233@gmail.com>
2016-06-22 16:02:34 +0300

    Fixed move semantics and implemented userdata variable in Window for custom user-specified data storage (as ptr)

--- src/Framework/Window/ImplWindow.cpp
+++ src/Framework/Window/ImplWindow.cpp
@@ -7,19 +7,31 @@
 using namespace DevaFramework;
 using ImplWindow = Window::ImplWindow;
 
-ImplWindow::ImplWindow(Window &wnd, uint32_t width, uint32_t height, const std::string &name)
-	:surface_height(height), name(name), eventObserver(std::shared_ptr<WindowObserver>(new WindowObserver()))
+ImplWindow::ImplWindow(Window *wnd, uint32_t width, uint32_t height, const std::string &name)
+	:wnd(wnd), surface_width(width), surface_height(height), name(name), eventObserver(std::shared_ptr<WindowObserver>(new WindowObserver()))
 {
 	this->impl_init();
 }
 
-ImplWindow::ImplWindow(ImplWindow &&implwnd)
-	:surface_width(implwnd.surface_width), surface_height(implwnd.surface_height), name(implwnd.name), eventObserver(std::move(implwnd.eventObserver))
+ImplWindow::ImplWindow(ImplWindow &&implwnd, Window* new_wnd)
+	:wnd(new_wnd), 
+	surface_width(implwnd.surface_width), 
+	surface_height(implwnd.surface_height), 
+	name(implwnd.name), 
+	eventObserver(std::move(implwnd.eventObserver)),
+	userData(implwnd.userData)
 {
 	this->impl_move(std::move(implwnd));
+	implwnd.wnd = nullptr;
 	implwnd.surface_width = 0;
 	implwnd.surface_height = 0;
 	implwnd.name = "";
+	implwnd.userData = nullptr;
+}
+
+ImplWindow::~ImplWindow()
+{
+	if(wnd != nullptr) impl_deInit();
 }
 
 WindowObserver& ImplWindow::getEventObserver()
@@ -38,9 +50,12 @@ void ImplWindow::close()
 	this->window_should_run = false;
 }
 
-ImplWindow::~ImplWindow()
+void* ImplWindow::getUserData() const
 {
-	impl_deInit();
+	return userData;
 }
 
-
+void ImplWindow::setUserData(void *userData)
+{
+	this->userData = userData;
+}
