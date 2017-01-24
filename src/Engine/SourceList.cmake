e96af96b1cb4d602d9638eb9bb678412740e480b
Stealthmate <stealthmate1233@gmail.com>
2017-01-24 22:49:33 +0200

    Added source list & public header generation

---
+++ src/Engine/SourceList.cmake
@@ -0,0 +1,31 @@
+set(
+	DEVA_ENGINE_SOURCE_LIST
+	${CURDIR}/GUI/Component.hpp
+	${CURDIR}/Input/Config.hpp
+	${CURDIR}/Input/InputFunction.cpp
+	${CURDIR}/Input/InputFunction.hpp
+	${CURDIR}/Input/InputListener.cpp
+	${CURDIR}/Input/InputListener.hpp
+	${CURDIR}/Input/KeyboardListener.cpp
+	${CURDIR}/Input/KeyboardListener.hpp
+	${CURDIR}/Input/MouseListener.hpp
+	${CURDIR}/Config.hpp
+	${CURDIR}/DevaEngineInstance.cpp
+	${CURDIR}/DevaEngineInstance.hpp
+	${CURDIR}/Init.cpp
+	${CURDIR}/Init.hpp
+	${CURDIR}/Keymap.hpp
+	${CURDIR}/Perspective.hpp
+	${CURDIR}/Renderer.cpp
+	${CURDIR}/Renderer.hpp
+	${CURDIR}/Scene.hpp
+	${CURDIR}/VulkanDevice.cpp
+	${CURDIR}/VulkanDevice.hpp
+	${CURDIR}/VulkanPipeline.hpp
+	${CURDIR}/VulkanPipelineBuilder.cpp
+	${CURDIR}/VulkanPipelineBuilder.hpp
+	${CURDIR}/VulkanRenderer.cpp
+	${CURDIR}/VulkanRenderer.hpp
+	${CURDIR}/VulkanSwapchain.cpp
+	${CURDIR}/VulkanSwapchain.hpp
+)
