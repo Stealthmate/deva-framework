29165ec440ae1b5a270ae9e4d4e61a616707d8bd
Stealthmate <stealthmate1233@gmail.com>
2017-03-19 20:15:55 +0200

    Add Uuid to Core master header

--- src/Framework/Include/Core.hpp
+++ src/Framework/Include/Core.hpp
@@ -1,7 +1,9 @@
-#ifndef DEVA_FRAMEWORK_CORE_HPP
-#define DEVA_FRAMEWORK_CORE_HPP
+#ifndef DEVA_FRAMEWORK_INCLUDE_CORE_HPP
+#define DEVA_FRAMEWORK_INCLUDE_CORE_HPP
 
 #include "..\Core\DevaLogger.hpp"
 #include "..\Core\Exceptions.hpp"
+#include "..\Core\Identifiable.hpp"
+#include "..\Core\Uuid.hpp"
 
-#endif // DEVA_FRAMEWORK_CORE_HPP
+#endif // DEVA_FRAMEWORK_INCLUDE_CORE_HPP
---
+++ src/Framework/Include/Math.hpp
@@ -0,0 +1,7 @@
+#ifndef DEVA_FRAMEWORK_INCLUDE_MATH_HPP
+#define DEVA_FRAMEWORK_INCLUDE_MATH_HPP
+
+#include "..\Math\Vector.hpp"
+#include "..\Math\Matrix.hpp"
+
+#endif // DEVA_FRAMEWORK_INCLUDE_MATH_HPP
