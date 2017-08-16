d8f08d71fa9d32f6028606a3f8ead0ccc49f7912
Stealthmate <stealthmate1233@gmail.com>
2017-08-16 19:42:49 +0900

    Fix code errors

--- tests/Framework/testMultiTypeVector.cpp
+++ tests/Framework/testMultiTypeVector.cpp
@@ -1,9 +1,5 @@
 #include <DevaFramework\Util\MultiTypeVector.hpp>
-#include <iostream>
-#include <DevaFramework\Core\Serializable.hpp>
-#include <DevaFramework\Core\Exceptions.hpp>
-
-
+#include <DevaFramework\Core\DevaLogger.hpp>
 #include <gtest\gtest.h>
 
 using namespace std;
@@ -22,19 +18,19 @@ TEST_F(Tester, testWriteRead) {
 	ASSERT_NO_THROW(packv.push_back({ 2, 2.3 }));
 	Pack<int, double> p = { 0, 0.0 };
 
-	cout << "Insertion works\n";
+	LOG << "Insertion works\n";
 
 	ASSERT_NO_THROW(p = packv[0]);
 	ASSERT_EQ(std::get<0>(p), 1);
 	ASSERT_EQ(std::get<1>(p), 2.2);
-
-	cout << "Extract 0 works\n";
+	
+	LOG << "Extract 0 works\n";
 
 	ASSERT_NO_THROW(p = packv[1]);
 	ASSERT_EQ(std::get<0>(p), 2);
 	ASSERT_EQ(std::get<1>(p), 2.3);
 
-	cout << "Extract 1 works\n";
+	LOG << "Extract 1 works\n";
 }
 
 int main(int argc, char **argv) {
