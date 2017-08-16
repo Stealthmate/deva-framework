021604486ac9e986ef0c1df5a59657d73ad3b63f
Stealthmate <stealthmate1233@gmail.com>
2017-08-17 01:46:20 +0900

    Rewrote generic Observer bases

--- src/Framework/Util/Observer.hpp
+++ src/Framework/Util/Observer.hpp
@@ -7,49 +7,71 @@
 
 namespace DevaFramework {
 
-	template<typename ObserverType>
-	class ObservableSubject {
+	template<typename T> class Observable;
+	template<typename T> class Observer;
+	template<typename T> void registerObserver(Observable<T> &observable, Observer<T> &observer) {
+		observable.registerObserver(observer);
+		observer.addTarget(observable);
+	}
+	template<typename T> void unregisterObserver(Observable<T> &observable, Observer<T> &observer) {
+		observable.unregisterObserver(observer);
+		observer.removeTarget(observable);
+	}
+
+	template<typename ObservableType>
+	class Observer {
 	public:
-		typedef std::weak_ptr<ObserverType> ObserverPtr;
+		virtual ~Observer() {
+			for (auto & t : targets) {
+				unregisterObserver(*t, *this);
+			}
+		}
+		
+		void onObservableDestroy(Observable<ObservableType> *o) {
+			targets.erase(std::find(targets.begin(), targets.end(), o));
+		}
 	private:
-		class ObserverPtrHasher {
-		public:
-			typedef ObserverPtr arg_t;
-			size_t operator()(const arg_t &arg) const {
-				return (uintptr_t)&arg;
-			}
-		};
-		class ObserverPtrEqualto {
-		public:
-			typedef ObserverPtr arg_t;
-			bool operator()(const arg_t &lhs, const arg_t &rhs) const {
-				return &lhs == &rhs;
-			}
-		};
+		std::list<Observable<ObservableType>*> targets;
+
+		void addTarget(Observable<ObservableType> &t) {
+			targets.push_back(&t);
+		}
+
+		void removeTarget(Observable<ObservableType> &t) {
+			targets.erase(std::find(targets.begin(), targets.end(), &t));
+		}
+
+		friend void DevaFramework::registerObserver(Observable<ObservableType>&, Observer<ObservableType>&);
+		friend void DevaFramework::unregisterObserver(Observable<ObservableType> &, Observer<ObservableType>&);
+	};
+
+	template<typename ObservableType>
+	class Observable {
 	public:
-		void registerObserver(ObserverPtr observer) const {
-			mObservers.insert(observer);
-		}
-		void unregisterObserver(ObserverPtr observer) const {
-			mObservers.erase(observer);
-		}
+		typedef Observer<ObservableType>* ObserverPtr;
 
-		void purge() const {
-			for (auto o = mObservers.begin(); o != mObservers.end();) {
-				if (o->expired()) o = mObservers.erase(o);
-				else o++;
-			}
-		}
-
-		const std::unordered_set<ObserverPtr, ObserverPtrHasher, ObserverPtrEqualto>& getObservers() const {
+		const std::list<ObserverPtr>& getObservers() const {
 			return mObservers;
 		}
 
-		virtual ~ObservableSubject() {}
+		virtual ~Observable() {
+			for (auto o : mObservers) {
+				o->onObservableDestroy(this);
+			}
+		}
 
 	private:
-		mutable std::unordered_set<ObserverPtr, ObserverPtrHasher, ObserverPtrEqualto> mObservers;
+		std::list<ObserverPtr> mObservers;
 
+		void registerObserver(Observer<ObservableType> &o) {
+			mObservers.push_back(&o);
+		}
+		void unregisterObserver(const Observer<ObservableType> &o) {
+			mObservers.erase(std::find(mObservers.begin(), mObservers.end(), &o));
+		}
+
+		friend void DevaFramework::registerObserver(Observable<ObservableType>&, Observer<ObservableType>&);
+		friend void DevaFramework::unregisterObserver(Observable<ObservableType> &, Observer<ObservableType>&);
 	};
 }
 
