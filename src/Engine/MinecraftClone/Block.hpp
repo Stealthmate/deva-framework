2e466895a7a1a3020826e940f1859c928b1afc68
Stealthmate <stealthmate1233@gmail.com>
2017-08-27 13:41:07 +0900

    Add play-around files

---
+++ src/Engine/MinecraftClone/Block.hpp
@@ -0,0 +1,24 @@
+#ifndef DEVA_MC_CLONE_BLOCK_HPP
+#define DEVA_MC_CLONE_BLOCK_HPP
+
+#include <DevaEngine\Graphics\DrawableObject.hpp>
+
+namespace Minecraft {
+
+	using namespace DevaFramework;
+	using namespace DevaEngine;
+
+
+	class Block {
+
+	public:
+
+
+	private:
+		DrawableObject drawable;
+		vec4 logicalBlock;
+	};
+
+}
+
+#endif //DEVA_MC_CLONE_BLOCK_HPP
