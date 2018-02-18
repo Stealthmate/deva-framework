b60b8e36f421b6e518f652c28749102ff6a27c88
Stealthmate <stealthmate1233@gmail.com>
2018-02-18 23:38:33 +0900

    Replace local vulkan files with settings specifiable in config

--- Win64.bat
+++ Win64.bat
@@ -2,5 +2,5 @@
 py gen.py
 mkdir Win64
 cd Win64
-cmake -DDEVA_BUILD_SPLIT:BOOL=FALSE -G "Visual Studio 15 Win64" ..
+cmake -DVK_LIB_PATH_D:STRING="C:\VulkanSDK\1.0.65.1\build\loader\Debug" -DVK_LIB_PATH:STRING="C:\VulkanSDK\1.0.65.1\build\loader\Release" -DDEVA_BUILD_SPLIT:BOOL=FALSE -G "Visual Studio 15 Win64" ..
 cd ..
